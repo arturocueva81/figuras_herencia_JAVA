0eb0351b143165567b6f796566dd7656f40874b0
arturocueva81 <ajcs81@hotmail.com>
2026-09-23 12:08:17 -0500

    Parte 2 - Prueba Inicial, clase TestFiguras, prueba inicial

---
+++ src/com/krakedev/test/TestFiguras.java
@@ -0,0 +1,24 @@
+package com.krakedev.test;
+
+import com.krakedev.figuras.Cuadrado;
+import com.krakedev.figuras.Figura;
+import com.krakedev.figuras.Triangulo;
+
+public class TestFiguras {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba Inicial:");
+		
+		Figura fig = new Figura("Figura","Rojo");
+		System.out.println(fig);
+		
+		Cuadrado cuad = new Cuadrado("Cuadro","Azul");
+		System.out.println(cuad);
+		
+		Triangulo tri = new Triangulo("Triangulo","Amarillo");
+		System.out.println(tri);
+
+	}
+
+}
